1c27c3fc7daed7ca406cf2fb2223d93ecab75b19
Hala-45 <77001975+Hala-45@users.noreply.github.com>
2022-02-14 07:16:11 +0200

    Revert "Client UI to update css"

--- ClientSide/src/main/java/org/iti/project/presentation/controllers/SideContactListController.java
+++ ClientSide/src/main/java/org/iti/project/presentation/controllers/SideContactListController.java
@@ -3,7 +3,6 @@ package org.iti.project.presentation.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.shape.Circle;
 
@@ -23,9 +22,4 @@ public class SideContactListController {
 
     @FXML
     public TextField searchBar;
-
-    @FXML
-    void test(MouseEvent event) {
-
-    }
 }
