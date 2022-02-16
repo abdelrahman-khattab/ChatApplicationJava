93422f977b8c0a8235b537ae899aabd9a450dcf4
Hala Tarek <halatarek80@gmail.com>
2022-02-16 18:29:35 +0200

    User Entity

---
+++ ClientSide/src/main/java/org/iti/project/presentation/models/User.java
@@ -0,0 +1,80 @@
+package org.iti.project.presentation.models;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String userName;
+    private String userPassword;
+    private String userEmail;
+    private String userBio;
+    private String userCountry;
+    private String userDOB;
+    private String userDate;
+    private String userPhone;
+    // Adding array of bytes for image
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserBio() {
+        return userBio;
+    }
+
+    public void setUserBio(String userBio) {
+        this.userBio = userBio;
+    }
+
+    public String getUserCountry() {
+        return userCountry;
+    }
+
+    public void setUserCountry(String userCountry) {
+        this.userCountry = userCountry;
+    }
+
+    public String getUserDOB() {
+        return userDOB;
+    }
+
+    public void setUserDOB(String userDOB) {
+        this.userDOB = userDOB;
+    }
+
+    public String getUserDate() {
+        return userDate;
+    }
+
+    public void setUserDate(String userDate) {
+        this.userDate = userDate;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+
+}
