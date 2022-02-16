04da372350b181a795b8b5904588c3b96178d0c7
A.Khattab <abdokhattab5@gmail.com>
2022-02-16 06:33:48 +0200

    Khattab Update ignore file and add mysql Dependency in pom file  in server side

--- ServerSide/src/main/java/module-info.java
+++ ServerSide/src/main/java/module-info.java
@@ -5,6 +5,9 @@ module ServerSide {  // change this to gov.iti.jets.project if needed
     requires de.jensd.fx.glyphs.fontawesome;
     requires de.jensd.fx.glyphs.commons;
     //requires org.controlsfx.controls;
+    requires java.sql;
+    requires mysql.connector.java;
+    requires java.naming;
 
     opens gov.iti.jets.project to javafx.fxml;
     exports gov.iti.jets.project;
